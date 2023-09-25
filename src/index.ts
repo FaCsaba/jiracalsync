5c349f82ab904cee2bf9850442cfe4c5ef17e5ea
FaCsaba <csab.faz.2@gmail.oom>
2023-09-25 11:21:36 +0200

    now using the '--watch' arg the program will perform sync every day

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,32 @@ require("dotenv").config();
 import { Jira } from "./jira";
 import { ICal } from "./ical";
 import { Mapper } from "./mapper";
+import { scheduleJob } from "node-schedule";
+import { argv } from "process";
+import { loggerFactory } from "./logger";
+import { LogType } from "./models/log_type.model";
 
 const ical = new ICal();
 const jira = new Jira();
 const mapper = new Mapper(jira);
+const log = loggerFactory('SYNC');
 
 async function main() {
+  log(LogType.Inf, "=== Starting sync ===")
   const events = await ical.getEvents();
 
-  const uniqueIssueKeys = [...new Set(events.map(e => e.issueKey))];
-  const worklogs = (await Promise.all(uniqueIssueKeys.map(async (issueKey) => await jira.getWorklogsByIssueKey(issueKey)))).flat();
+  const uniqueIssueKeys = [...new Set(events.map((e) => e.issueKey))];
+  const worklogs = (
+    await Promise.all(
+      uniqueIssueKeys.map(
+        async (issueKey) => await jira.getWorklogsByIssueKey(issueKey)
+      )
+    )
+  ).flat();
 
   mapper.mapEventsToWorklogs(events, worklogs);
 }
 
 main().then();
+if (argv.includes("--watch"))
+  scheduleJob("0 0 * * *", async () => main);
