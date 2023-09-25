dd278fd0eaa62f1097df31c8c008dd5424066488
FaCsaba <csab.faz.2@gmail.oom>
2023-09-25 11:15:47 +0200

    now logging substitutions

--- src/ical.ts
+++ src/ical.ts
@@ -14,6 +14,7 @@ export class ICal {
     private log = loggerFactory("ICAL"),
   ) {
     log(LogType.Inf, `ICAL_PRIVATE_URL = ${this.calURL}`);
+    log(LogType.Inf, `SUBS = ${JSON.stringify(this.subs)}`)
   }
 
   /**
